7a9c4a6f02e08674f3c2e771882fa1ecd432421e
OCreativeTeen <80544024+OCreativeTeen@users.noreply.github.com>
2021-03-13 12:23:37 -0500

    init commit

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    public void Start()
+    {
+        Debug.Log("Camera Movement is started");
+    }
+
+    public void Update() {
+        this.yaw = this.yaw + this.getMouseMove("Mouse X");
+        this.pitch = this.pitch - this.getMouseMove("Mouse Y");
+
+        this.transform.eulerAngles = new Vector3(this.pitch, this.yaw, 0.0f);
+    }
+
+
+    private float getMouseMove(string flag) {
+        return Input.GetAxis(flag);
+    }
+
+
+}
---
+++ Assets/Script/Ship.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+
+class Ship : MonoBehaviour {
+
+    public string shipName = "TITANIC";
+
+    public int shipBuildYear = 1911;
+
+    public float rotateSpeed = 1.2f;
+
+    public bool anchorOn = false;
+
+
+
+    public GameObject center;
+
+
+
+    void Awake() {
+        Debug.Log("Ship (" + shipName + ") is just generated into the game");
+    }
+
+
+    void Start() {
+        Debug.Log("Ship (" + shipName + ") is started to run in the game");
+    }
+
+
+    // Update is called every 0.04 second (around)
+    void Update() {
+        if (!anchorOn) {
+            Vector3 line = new Vector3(0f, 1f, 0f);  // Can NOT say "this.line"
+            RotateAround(line);
+        }
+    }
+
+
+    void RotateAround(Vector3 line) {
+        transform.RotateAround( this.center.transform.position, line, rotateSpeed*Time.deltaTime );
+    }
+
+
+}
